92e5bbc0c616c86b76ae39c69ed4fd9707f3766f
naveenkumarcsem <naveenkumar0072001@gmail.com>
2022-06-26 16:04:51 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using FileuploadMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileuploadMVC.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(Employee employee)
+        {
+            using (controlEntities entity = new controlEntities())
+            {
+                var candidate = new Candidate()
+                {
+                    ContactNo = employee.ContactNo,
+                    EmailID = employee.EmailID,
+                    FirstName = employee.FirstName,
+                    LastName = employee.LastName,
+                    Position = employee.Position,
+                    Resume = SaveToPhysicalLocation(employee.Resume),
+                    Skills = employee.Skills,
+                    CreatedOn = DateTime.Now
+                };
+                entity.Candidates.Add(candidate);
+                entity.SaveChanges();
+                
+            }
+            return View(employee);
+        }
+
+        
+
+        /// <summary>
+        ///  Save Posted File in Physical path and return saved path to store in a database
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        private string SaveToPhysicalLocation(HttpPostedFileBase file)
+        {
+            if (file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/App_Data"), fileName);
+                file.SaveAs(path);
+                return path;
+            }
+            return string.Empty;
+        }
+    }
+}
+
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FileuploadMVC.MvcApplication" Language="C#" %>
